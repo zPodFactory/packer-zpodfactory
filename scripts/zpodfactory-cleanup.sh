eb9c38e319da652e06dd7b753ea461ef23e48565
Timo Sugliani <timo.sugliani@gmail.com>
2025-04-27 13:38:52 +0000

    nonsensical blabla

--- scripts/zpodfactory-cleanup.sh
+++ scripts/zpodfactory-cleanup.sh
@@ -54,8 +54,6 @@ rm -f /var/lib/dhcp/*
 # really make certain that both the zeros and the file removal really sync; the
 # extra sleep 1 and sync shouldn't be necessary, but...)
 
-
-## FIXME: commented out for now, slowest step.
 echo '> Zeroing device to reduce resulting VMDK & OVA export...'
 dd if=/dev/zero of=/EMPTY bs=1M || true; sync; sleep 1; sync
 rm -f /EMPTY; sync; sleep 1; sync
